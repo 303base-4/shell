59cd54e2a16f6b2b2c8f9f9edfe5a10461f26b5f
Anlor <chenxiex@outlook.com>
2023-07-03 15:01:43 +0800

    修复log.c中的部分问题

--- src/log.c
+++ src/log.c
@@ -1,85 +1,102 @@
 /** @file log.c */
+#include "log.h"
 #include <stdlib.h>
 #include <string.h>
-#include "log.h"
 
 /**
 å¯¹logè¿›è¡Œåˆå§‹åŒ–ï¼Œlogçš„â€œæž„é€ å‡½æ•°â€
  */
 
-void log_init(log_t *&l) {
-	l=(log_t*)malloc(sizeof(log_t));
-    l->head=NULL;
+void log_init(log_t *l)
+{
+    l->head = NULL;
 }
 
 /**
-Ïú»ÙÒ»¸ölog£¬ÊÍ·ÅËùÓÐÄÚ´æ¿Õ¼ä£¬logµÄ¡°Îö¹¹º¯Êý¡±
+ï¿½ï¿½ï¿½ï¿½Ò»ï¿½ï¿½logï¿½ï¿½ï¿½Í·ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ú´ï¿½Õ¼ä£¬logï¿½Ä¡ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
  */
 
-void log_destroy(log_t* l) {
-    node *p,*p1;
-    p1=l->head;
-    p=p1->next;
-    while(p!=NULL){
+void log_destroy(log_t *l)
+{
+    node *p, *p1;
+    p1 = l->head;
+    p = p1->next;
+    while (p != NULL)
+    {
         free(p1->cmd);
         free(p1);
-        p1=p;
-        p=p1->next;
+        p1 = p;
+        p = p1->next;
     }
     free(p1->cmd);
     free(p1);
 }
 
 /**
-ÏòlogÖÐÍÆÈëÒ»¸ö×Ö·û´®£¬Äã¿ÉÒÔ½«logÊÓÎªÒ»¸öÓÉÁ´±í×é³ÉµÄÕ»
+ï¿½ï¿½logï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ò»ï¿½ï¿½ï¿½Ö·ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ô½ï¿½logï¿½ï¿½ÎªÒ»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Éµï¿½Õ»
  */
 
-void log_push(log_t* l, const char *item) {
-    if(l->head==NULL){
+void log_push(log_t *l, const char *item)
+{
+    if (l->head == NULL)
+    {
         node *p;
-        p = (node*)malloc(sizeof(node));
-        p->cmd=(char*)malloc(100); 
-        p->next=NULL;
-        strcpy(p->cmd,item);
-        l->head=p;
+        p = (node *)malloc(sizeof(node));
+        p->cmd = (char *)malloc((strlen(item) + 1) * sizeof(char));
+        p->next = NULL;
+        strcpy(p->cmd, item);
+        l->head = p;
     }
-    else{
+    else
+    {
         node *p1;
-        p1=l->head;
-        while(p1->next!=NULL){
-            p1=p1->next;
+        p1 = l->head;
+        while (p1->next != NULL)
+        {
+            p1 = p1->next;
         }
         node *p;
-        p = (node*)malloc(sizeof(node));
-        strcpy(p->cmd,item);
-        p->next=p1->next;
-        p1->next=p;
+        p = (node *)malloc(sizeof(node));
+        p->cmd = (char *)malloc((strlen(item) + 1) * sizeof(char));
+        strcpy(p->cmd, item);
+        p->next = p1->next;
+        p1->next = p;
     }
 }
 
-
 /**
-ËÑË÷logÖÐÊÇ·ñº¬ÓÐ¶ÔÓ¦Ç°×ºµÄ×Ö·û´®
+ï¿½ï¿½ï¿½ï¿½logï¿½ï¿½ï¿½Ç·ï¿½ï¿½Ð¶ï¿½Ó¦Ç°×ºï¿½ï¿½ï¿½Ö·ï¿½ï¿½ï¿½
  */
 
-char *log_search(log_t* l, const char *prefix) {
-    node *p=l->head;
-    while(p!=NULL){
+char *log_search(log_t *l, const char *prefix)
+{
+    node *p = l->head;
+    while (p != NULL)
+    {
         char *s;
-        s=p->cmd;
-        int i,flag=0;
-        while(prefix[i]!=0){
-        	if(prefix[i]!=s[i]){
-        		flag++;
-        		break;
-			}
-			i++;
-		}
-        if(flag==0){
+        s = p->cmd;
+        int i = 0, flag = 0;
+        if (strlen(prefix) > strlen(s))
+        {
+            p = p->next;
+            continue;
+        }
+        while (prefix[i] != '\0')
+        {
+            if (prefix[i] != s[i])
+            {
+                flag = 1;
+                break;
+            }
+            i++;
+        }
+        if (flag == 0)
+        {
             return p->cmd;
         }
-        else{
-            p=p->next;
+        else
+        {
+            p = p->next;
         }
     }
     return NULL;
